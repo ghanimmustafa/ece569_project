0097c79d9d3750a7912feee91af249a0f66ab8cc
ghanimmustafa <mustafaghanim@arizona.edu>
2023-04-29 16:58:57 -0700

    updating sweeping script for different versions of LBM

--- cuda/sweep.sh
+++ cuda/sweep.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+VERSIONS=("0" "1" "2" "3")
 # Define the input image directory
-IMG_DIR="./img/Motorcycle/"
+IMG_DIR=("./img/Motorcycle")
 DISPARITY_RANGE=("270")
 
 # Define the gpu_block configurations
-GPU_BLOCKS=("1" "2" "4" "8" "16" "32")
+GPU_BLOCKS=("2" "4" "8" "16" "32")
 
 # Define the SAD_block configurations
-SAD_BLOCKS=("3" "5" "7" "9" "11")
+SAD_BLOCKS=("3" "7" "11" "15")
 
 # Loop over the image names, gpu_block configurations, and SAD_block configurations
-
-for GPU_BLOCK in "${GPU_BLOCKS[@]}"
+for SAD_BLOCK in "${SAD_BLOCKS[@]}"
 do
-    for SAD_BLOCK in "${SAD_BLOCKS[@]}"
-    do
-        # Run the lbm program with the current configuration
-        ./lbm $GPU_BLOCK $SAD_BLOCK $DISPARITY_RANGE $IMG_DIR
-    done
-done
+	for GPU_BLOCK in "${GPU_BLOCKS[@]}"
+	do
 
+			for VERSION in "${VERSIONS[@]}"
+				do
+		      # Run the lbm program with the current configuration
+		      ./lbm $VERSION $GPU_BLOCK $SAD_BLOCK $DISPARITY_RANGE $IMG_DIR
+		  done
+	done
+done
 
